7abcf10ed0da9408e215febb7267593516cc9954
Alex Rodriguez <xalecr30@gmail.com>
2024-06-15 10:24:16 -0400

    cambio de url

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const codeigniter = axios.create({
     
-    'baseURL': 'http://18.226.82.14/index.php/api/v1/',
+    'baseURL': 'https://alexder.codes/Backend-tecnohub/index.php/api/v1',
     //baseURL': 'http://localhost/backend-proyecto/index.php/api/v1',
     'headers' : { 
         'Access-Control-Allow-Origin': '*',
