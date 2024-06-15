4daf529894cdb4fd3a94d962b72d706b3c39d35b
Alex Rodriguez <xalecr30@gmail.com>
2024-06-15 01:55:37 -0400

    ignore ssl

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,13 +1,17 @@
 import axios from "axios";
 
 const codeigniter = axios.create({
-    'baseURL': 'http://ec2-18-191-66-28.us-east-2.compute.amazonaws.com/index.php/api/v1',
+    
+    'baseURL': 'http://18.226.82.14/index.php/api/v1/',
     //baseURL': 'http://localhost/backend-proyecto/index.php/api/v1',
     'headers' : { 
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method",
         'Access-Control-Allow-Methods' :'GET, POST PUT, DELETE',
-    }
+    },
+    httpsAgent: new https.Agent({  
+        rejectUnauthorized: false
+    })
 });
 
 export default codeigniter;
